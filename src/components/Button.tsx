9197d5b9979c85bb2976ded1a44d2c6caa1662de
Rodrigo Narvaez <rodadev47@gmail.com>
2024-02-02 23:01:45 -0500

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React, { PropsWithChildren } from 'react'
+
+export interface ButtonProps
+  extends PropsWithChildren,
+    React.HTMLAttributes<HTMLButtonElement> {}
+
+const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => {
+  return (
+    <button className="rounded bg-blue-500 px-4 py-2 text-white">
+      {children}
+    </button>
+  )
+}
+
+export default Button
