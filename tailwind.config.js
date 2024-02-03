f1a34e6feb88b6592dbdcd1554da3e7044862b6c
Rodrigo Narvaez <rodadev47@gmail.com>
2024-02-03 00:03:24 -0500

    finished with tailwind and sb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
-  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: ['class'],
   theme: {
     extend: {},
   },
