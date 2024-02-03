9bbedde855d013d10ec22f5c0589be552118ac5b
Rodrigo Narvaez <rodadev47@gmail.com>
2024-02-02 23:51:08 -0500

    add themes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-styling-webpack',
+    '@storybook/addon-themes',
     {
       name: '@storybook/addon-styling-webpack',
 
